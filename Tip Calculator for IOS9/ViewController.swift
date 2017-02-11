396e572e184ce155cdcf041573dcfa357a3907d3
Rubén Antonio Martinez Pacheco <ruben.martinez@definityfirst.com>
2017-02-11 13:36:14 -0600

    Initial Commit

---
+++ Tip Calculator for IOS9/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tip Calculator for IOS9
+//
+//  Created by Ruben Martinez on 11/02/17.
+//  Copyright © 2017 Ruben Martinez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
